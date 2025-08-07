8067465c98d1ed28f1e4104f36ae6cbf31474d03
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2025-08-07 16:57:48 +0200

    added tailwindcss

--- cmd/src/app/page.tsx
+++ cmd/src/app/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import StatusLabel, { Status } from './components/active-label/status-label';
+
+
 
 export default function Home() {
   return (
-   <main>
-    <h1>Home page</h1>
+   <main >
+    <h1 className="text-xl, m:20">Home page</h1>
+    <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
+        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+          <StatusLabel status={Status.Suspended}>Supended</StatusLabel>
+   
    </main>
 
   );
