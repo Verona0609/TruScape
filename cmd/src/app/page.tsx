fa1402ab9ee39cda09ec0e72347dd2383b8cd289
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2025-08-08 16:29:01 +0200

    dynamic rendering

--- cmd/src/app/page.tsx
+++ cmd/src/app/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import StatusLabel, { Status } from './components/active-label/status-label';
+import {headers} from 'next/headers';
 
 
 
 export default function Home() {
+  console.log(headers())
+  
   return (
    <main >
-    <h1 className="text-xl, m:20">Home page</h1>
+    <h1 className="text-xl, m:20">Home page {new Date().toTimeString()}</h1>
     <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
         <StatusLabel status={Status.Pending}>Pending</StatusLabel>
